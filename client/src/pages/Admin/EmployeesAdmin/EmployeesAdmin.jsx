c6ee95cb594ab17a7bcc85074add07a18b6c6d0e
yelnur <114299301+yelnurq@users.noreply.github.com>
2026-04-11 22:28:28 +0500

    Update EmployeesAdmin.jsx

--- client/src/pages/Admin/EmployeesAdmin/EmployeesAdmin.jsx
+++ client/src/pages/Admin/EmployeesAdmin/EmployeesAdmin.jsx
@@ -75,7 +75,6 @@ export default function EmployeesAdmin() {
               Добавить мастера
             </button>
           </div>
-
           {isLoading ? (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {[1, 2, 3].map(i => (
